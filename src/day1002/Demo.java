ea34241bf3fa5f47ed84db1fc3a3dc745a7be3a0
wujianjian <wujianjian@192.168.10.1>
2019-08-24 11:35:31 +0800

    1111

---
+++ src/day1002/Demo.java
@@ -0,0 +1,45 @@
+package day1002;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		f();
+		
+		
+		
+		
+		
+	}
+
+	public static void f() {
+		
+		int a = new Random().nextInt();
+		int b = new Random().nextInt();
+		int c = a+b;
+		System.out.println("a+b=");
+		System.out.println(c);
+	}
+
+	
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
---
+++ src/day1002/Test.java
@@ -0,0 +1,12 @@
+package day1002;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
